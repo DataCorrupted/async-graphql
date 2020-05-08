c289f183b0ca44bc7742810a87b339c3912bbeff
Sunli <scott_s829@163.com>
2020-05-08 12:03:31 +0800

    a

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,3 +1,20 @@
+use pest::Parser;
+
 #[derive(Parser)]
 #[grammar = "parser/query.pest"]
 struct QueryParser;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parser_int() {
+        assert_eq!(
+            QueryParser::parse(Rule::int, "123").unwrap().as_str(),
+            "123"
+        );
+        assert_eq!(QueryParser::parse(Rule::int, "0").unwrap().as_str(), "0");
+        println!("{:?}", QueryParser::parse(Rule::int, "0123").unwrap());
+    }
+}
